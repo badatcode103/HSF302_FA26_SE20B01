72a7b16ad851aa74054370d09855c48b5c52f286
HuuMinh <chikeratop@gmail.com>
2026-09-11 13:27:06 +0700

    1st commit

---
+++ src/main/java/org/example/hsf302_classproject_1/model/enumPackage/Gender.java
@@ -0,0 +1,7 @@
+package org.example.hsf302_classproject_1.model.enumPackage;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/test/java/org/example/hsf302_classproject_1/Hsf302ClassProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hsf302_classproject_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hsf302ClassProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
