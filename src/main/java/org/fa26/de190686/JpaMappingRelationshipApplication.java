decb13ba9d826b8f3778d30361e4ec50badb17db
HuuMinh <chikeratop@gmail.com>
2026-09-15 16:12:43 +0700

    feat(slot3): config project

---
+++ src/main/java/org/fa26/de190686/JpaMappingRelationshipApplication.java
@@ -0,0 +1,13 @@
+package org.fa26.de190686;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMappingRelationshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMappingRelationshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fa26/de190686/JpaMappingRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fa26.de190686;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMappingRelationshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
