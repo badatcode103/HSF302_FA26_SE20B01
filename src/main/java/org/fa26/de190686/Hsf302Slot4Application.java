87681a13b5a1cfe36e5fd1f8888ec2d907af31d3
HuuMinh <chikeratop@gmail.com>
2026-09-18 13:28:47 +0700

    initial commit

---
+++ src/main/java/org/fa26/de190686/Hsf302Slot4Application.java
@@ -0,0 +1,13 @@
+package org.fa26.de190686;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hsf302Slot4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hsf302Slot4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/fa26/de190686/Hsf302Slot4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fa26.de190686;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hsf302Slot4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
